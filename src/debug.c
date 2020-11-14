95a044a7420a66513bddd2bd09208faec4122721
Chuck Tilbury <chucktilbury@gmail.com>
2020-11-13 20:56:13 -0600

    Finished with chapter 22

--- src/debug.c
+++ src/debug.c
@@ -13,8 +13,16 @@ void disassembleChunk(Chunk* chunk, const char* name) {
 }
 
 static int simpleInstruction(const char* name, int offset) {
+
     printf("%s\n", name);
     return offset + 1;
+}
+
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+
+      uint8_t slot = chunk->code[offset + 1];
+      printf("%-16s %4d\n", name, slot);
+      return offset + 2;
 }
 
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
@@ -51,6 +59,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_POP:
             return simpleInstruction("OP_POP", offset);
 
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk, offset);
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk, offset);
         case OP_DEFINE_GLOBAL:
             return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
         case OP_GET_GLOBAL:
